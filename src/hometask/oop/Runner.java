5403ef22bd12911d0599182680dc77634850f598
horodniuk <horodniuk.maksym55@gmail.com>
2022-10-09 17:36:32 +0300

    add runner

---
+++ src/hometask/oop/Runner.java
@@ -0,0 +1,8 @@
+package hometask.oop;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Employee("Ivan", "driver", "ivanov@gmail.com", "80991111111", 25);
+        new Car().start();
+    }
+}
