ff607c8a98cbfbbb10b23403bcb116a3bc264332
horodniuk <horodniuk.maksym55@gmail.com>
2022-11-02 17:42:28 +0200

    fix hometask 11 multithreading

--- src/hometask/multithreading/value_calculator/ValueCalculator.java
+++ src/hometask/multithreading/value_calculator/ValueCalculator.java
@@ -1,54 +1,39 @@
 package hometask.multithreading.value_calculator;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class ValueCalculator {
-    private final int size = 1000000;
-    private final int half = size / 2;
-    private final float[] arr = new float[size];
+    private final int sizeArray = 1000000;
+    private final int halfSizeArray = sizeArray / 2;
+    private final float[] array = new float[sizeArray];
 
     public void doCalc() {
         long start = System.currentTimeMillis();
-        Arrays.fill(arr, 1);
+        Arrays.fill(array, 1);
 
-        float[] a1 = new float[half];
-        float[] a2 = new float[half];
-        System.arraycopy(arr, 0, a1, 0, half);
-        System.arraycopy(arr, half, a2, 0, half);
+        float[] firstHalfArray = new float[halfSizeArray];
+        float[] secondHalfArray = new float[halfSizeArray];
+        System.arraycopy(array, 0, firstHalfArray, 0, halfSizeArray);
+        System.arraycopy(array, halfSizeArray, secondHalfArray, 0, halfSizeArray);
 
-        ExecutorService executor = Executors.newFixedThreadPool(3);
-        List<Future<?>> futures = new ArrayList<>();
-        futures.add(executor.submit(new Thread(changeValues(a1))));
-        futures.add(executor.submit(new Thread(changeValues(a2))));
-
-        for (Future<?> f : futures) {
-            try {
-                f.get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
+        Thread threadForFirstArray = new Thread(changeValues(firstHalfArray));
+        Thread threadForSecondArray = new Thread(changeValues(secondHalfArray));
+        threadForFirstArray.start();
+        threadForSecondArray.start();
+        try {
+            threadForFirstArray.join();
+            threadForSecondArray.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        executor.shutdown();
 
-        System.arraycopy(a1, 0, arr, 0, half);
-        System.arraycopy(a2, 0, arr, half, half);
-
+        System.arraycopy(firstHalfArray, 0, array, 0, halfSizeArray);
+        System.arraycopy(secondHalfArray, 0, array, halfSizeArray, halfSizeArray);
         System.out.printf("time execute program - %s milliseconds%n", System.currentTimeMillis() - start);
     }
 
     private Runnable changeValues(float[] arr) {
-        return new Runnable() {
-            @Override
-            public void run() {
-                ValueCalculator.this.assignNewValuesByFormula(arr);
-            }
-        };
+        return () -> assignNewValuesByFormula(arr);
     }
 
     private void assignNewValuesByFormula(float[] arr) {
