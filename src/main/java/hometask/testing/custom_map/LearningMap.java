37e79854119a9416e3b079f6e22471111c4a13c5
horodniuk <horodniuk.maksym55@gmail.com>
2022-11-29 00:15:48 +0200

    init task learning map

---
+++ src/main/java/hometask/testing/custom_map/LearningMap.java
@@ -0,0 +1,7 @@
+package hometask.testing.custom_map;
+
+public interface LearningMap {
+    void put(Integer key, String value);
+
+    String get(Integer key);
+}
---
+++ src/main/java/hometask/testing/custom_map/LearningMapImpl.java
@@ -0,0 +1,13 @@
+package hometask.testing.custom_map;
+
+public class LearningMapImpl implements LearningMap {
+    @Override
+    public void put(Integer key, String value) {
+
+    }
+
+    @Override
+    public String get(Integer key) {
+        return null;
+    }
+}
